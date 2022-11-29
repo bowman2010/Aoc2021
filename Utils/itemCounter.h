cfd05fa29f6f94da209ce468ab410a907d92024a
Fvanderpo <francois.vanderpoorte@gmail.com>
2022-11-29 09:32:38 +0100

    new functions high,low,highest,lowests

--- Utils/itemCounter.h
+++ Utils/itemCounter.h
@@ -2,6 +2,9 @@
 #define ITEMCOUNTER_H
 
 #include <map>
+#include <vector>
+#include <climits>
+
 namespace bml {
 
 /**
@@ -15,28 +18,34 @@ protected:
 public:
     /**
      * @brief add : add or increase the counter of item
+     * @param count : how many are added
      * @param item
      */
-    unsigned add(const T &item) {
+    unsigned add(const T &item, unsigned count=1) {
         auto itr = counters.find(item);
         if (itr==counters.end()) {
-            counters.insert(std::make_pair(item,1));
-        } else itr->second++;
+            counters.insert(std::make_pair(item,count));
+        } else itr->second+=count;
         return itr->second;
     }
 
     /**
      * @brief remove : decrease the count of item
      * @param item
+     * @param count : how many are removed
+     * @param del : if true, element is removed from map else count set to zero
      */
-    unsigned remove(const T &item) {
+    unsigned remove(const T &item, unsigned count=1, bool del=true) {
         auto itr = counters.find(item);
         if (itr!=counters.end()) {
-            itr->second--;
-            if (itr->second==0) {
-                counters.erase(item);
+            if (itr->second<=count) {
+                if (del) counters.erase(item);
                 return 0;
-            } else return itr->second;
+            }
+            else {
+                itr->second -= count;
+                return itr->second;
+            }
         }
         return 0;
     }
@@ -44,28 +53,100 @@ public:
     /**
      * @brief removeAll : set the count of item to zero
      * @param item
+     * @param del : if true, element is removed from map else set count to zero
      */
-    unsigned removeAll(const T &item) {
-        counters.erase(item);
+    unsigned removeAll(const T &item, bool del=true) {
+        auto itr = counters.find(item);
+        if (itr!=counters.end()) {
+            if (del) counters.erase(item);
+            else itr->second = 0;
+        }
         return 0;
+    }
+
+    /**
+     * @brief clear al counters
+     * @param del : if true, elements areremoved from map else their count are set to zero
+     */
+    void reset(bool del=true)
+    {
+        if (!del) {
+            counters.clear();
+        } else {
+            for (auto p = counters.begin(); p<counters.end; p++)
+                p->second=0;
+        }
     }
 
     /**
      * @brief count : the count of item
      * @param item
      */
-    unsigned count(const T &item) {
+    unsigned count(const T &item) const {
         auto itr = counters.find(item);
         if (itr==counters.end()) return 0;
         return itr->second;
     }
 
     /**
-     * @brief clear al counters
+     * @brief low : lowest items count
+     * @return a vector of pairs of <item,counter>
      */
-    void clear()
-    {
-        counters.clear();
+    std::vector<std::pair<T&,unsigned>> lowests() const {
+        unsigned low = UINTMAX_MAX;
+        std::vector<std::pair<T&,unsigned>> vec;
+
+        for (auto itr=counters.begin(); itr<counters.end(); itr++)
+        {
+            if (itr->second==low) vec.push_back(*itr);
+            else if(itr->second < low) {
+                vec.clear();
+                vec.push_back(*itr);
+            }
+        }
+        return vec;
+    }
+
+    /**
+     * @brief high : highest count
+     * @return a vector of pairs of <item,counter>
+     */
+    std::vector<std::pair<T&,unsigned>> highests() const {
+        unsigned high = 0;
+        std::vector<std::pair<T&,unsigned>> vec;
+        for (auto itr=counters.begin(); itr<counters.end(); itr++)
+        {
+            if (itr->second==high) vec.push_back(*itr);
+            else if(itr->second > high) {
+                vec.clear();
+                vec.push_back(*itr);
+            }
+        }
+        return vec;
+   }
+
+    /**
+     * @brief low : get the lowest count
+     * @return an unsigned
+     */
+    unsigned low() const {
+        unsigned low = UINTMAX_MAX;
+        if (counters.size()==0) return 0;
+        for (auto itr=counters.begin(); itr<counters.end(); itr++)
+            if (itr->second < low) low=itr->second;
+        return low;
+    }
+
+    /**
+     * @brief high : get the highest count
+     * @return an unsigned
+     */
+    unsigned high() const {
+        unsigned high = 0;
+        if (counters.size()==0) return 0;
+        for (auto itr=counters.begin(); itr<counters.end(); itr++)
+            if (itr->second > high) high=itr->second;
+        return high;
     }
 };
 
--- Utils/main.cpp
+++ Utils/main.cpp
@@ -8,23 +8,17 @@ using namespace std;
 int main()
 {
     bml::ItemCounter<string> ic1;
-    ic1.add("AA");
-    ic1.add("AA");
-    ic1.add("BB");
-    ic1.add("CC");
-    cout << " count(AA) : " << ic1.count("AA") << endl;
-    cout << " count(BB) : " << ic1.count("BB") << endl;
-    cout << " count(CC) : " << ic1.count("CC") << endl;
+    ic1.add("AA",2);
+    ic1.add("BB",2);
+    ic1.add("CC",3);
+    ic1.add("DD",4);
+    ic1.add("EE",4);
 
-    cout << "removing AA and BB" << endl;
-    ic1.remove("AA"); ic1.remove("BB");
-    cout << " count(AA)  : " << ic1.count("AA") << endl;
-    cout << " count(BB) : " << ic1.count("BB") << endl;
-
-    cout << "removing AA and BB" << endl;
-    ic1.remove("AA"); ic1.remove("BB");
-    cout << " count(AA)  : " << ic1.count("AA") << endl;
-    cout << " count(BB) : " << ic1.count("BB") << endl;
+    ic1.remove("AA");
+    ic1.removeAll("BB",false);
+    ic1.remove("CC",4,false);
+    ic1.remove("DD",4);
+    ic1.remove("EE",56);
 
     return 0;
 }
